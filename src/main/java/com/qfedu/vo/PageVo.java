d9d896028a8f27c83502aa357941a3f4514413cc
geng <geng_weichao@163.com>
2019-02-20 11:12:58 +0800

    Initial commit 第一次提交SSM框架配置

---
+++ src/main/java/com/qfedu/vo/PageVo.java
@@ -0,0 +1,53 @@
+package com.qfedu.vo;
+
+import java.util.List;
+
+
+public class PageVo<T> {
+    private long count;
+    private int page;
+    private int size;
+    private int totalpage;
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getTotalpage() {
+        return totalpage;
+    }
+
+    public void setTotalpage(int totalpage) {
+        this.totalpage = totalpage;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+
+    private List<T> data;
+}
